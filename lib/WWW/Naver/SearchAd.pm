a950710f63546068f47da88c60094e2887829af1
Hyungsuk Hong <aanoaa@gmail.com>
2016-07-17 21:43:56 +0900

    Add some methods

--- lib/WWW/Naver/SearchAd.pm
+++ lib/WWW/Naver/SearchAd.pm
@@ -7,6 +7,7 @@ use Moo;
 use Digest::SHA qw(hmac_sha256_base64);
 use HTTP::Tiny;
 use Time::HiRes qw(gettimeofday);
+use JSON qw/encode_json/;
 
 our $BASE_URL = 'https://api.naver.com';
 
@@ -25,17 +26,26 @@ has http        => (
 
 =head2 request
 
-    my $data = $api->request('GET', '/ncc/campaigns');
+    my $res = $api->request($method, $path, $params?, $opts?);
 
 =cut
 
 sub request {
-    my ( $self, $method, $path ) = @_;
+    my ( $self, $method, $path, $params, $opts ) = @_;
 
-    print STDERR "--> Working on $method $path ... ";
+    my $url = $BASE_URL . $path;
+    $url .= "?$params" if $params;
+    print STDERR "--> Working on $method $url ... ";
 
-    my $res
-        = $self->http->request( $method, $BASE_URL . $path, { headers => $self->custom_headers( $method, $path ) } );
+    my $custom_headers = $self->custom_headers( $method, $path );
+    if ($opts) {
+        map { $opts->{headers}{$_} = $custom_headers->{$_} } keys %$custom_headers;
+    }
+    else {
+        $opts = { headers => $custom_headers };
+    }
+
+    my $res = $self->http->request( $method, $url, $opts );
 
     unless ( $res->{success} ) {
         print STDERR "Failed\n";
@@ -45,13 +55,13 @@ sub request {
     }
 
     print STDERR "OK\n";
-    return $res->{content};
+    return $res;
 }
 
 =head2 campagins
 
-    https://api.naver.com/ncc/campaigns
-    my $data = $api->campaigns(%attr);
+    GET /ncc/campaigns{?baseSearchId,recordSize,selector}
+    my $json = $api->campaigns(%attr);
 
 =cut
 
@@ -59,8 +69,107 @@ sub campaigns {
     my ( $self, $attr ) = @_;
 
     ## TODO: $attr
-    return $self->request( 'GET', '/ncc/campaigns' );
+    my $res = $self->request( 'GET', '/ncc/campaigns' );
+    return unless $res;
+    return $res->{content};
 }
+
+=head2 adgroups($campaignId)
+
+    GET /ncc/adgroups{?nccCampaignId,baseSearchId,recordSize,selector}
+    my $json = $api->adgroups($ids);
+
+=cut
+
+sub adgroups {
+    my ( $self, $campaignId ) = @_;
+
+    return unless $campaignId;
+
+    my $params = $self->http->www_form_urlencode( { nccCampaignId => $campaignId } );
+    my $res = $self->request( 'GET', '/ncc/adgroups', $params );
+    return unless $res;
+    return $res->{content};
+}
+
+=head2 keywords($ids)
+
+    GET /ncc/keywords{?nccAdgroupId,baseSearchId,recordSize,selector}
+    my $json = $api->keywords($ids);
+
+=cut
+
+sub keywords {
+    my ( $self, $adgroupId ) = @_;
+
+    return unless $adgroupId;
+
+    my $params = $self->http->www_form_urlencode( { nccAdgroupId => $adgroupId } );
+    my $res = $self->request( 'GET', '/ncc/keywords', $params );
+    return unless $res;
+    return $res->{content};
+}
+
+=head2 keyword($keywordId)
+
+    GET /ncc/keywords/{nccKeywordId}
+    my $json = $api->keyword($keywordId);
+
+=cut
+
+sub keyword {
+    my ( $self, $keywordId ) = @_;
+
+    return unless $keywordId;
+
+    my $res = $self->request( 'GET', "/ncc/keywords/$keywordId" );
+    return unless $res;
+    return $res->{content};
+}
+
+=head2 update_keyword($bidAmt, $useGroupBidAmt, $adgroupId)
+
+    PUT /ncc/keywords/{nccKeywordId}{?fields}
+    my $json = $api->update_keyword(550, 'false', 'grp-m001-01-0000000000001');
+
+=cut
+
+sub update_keyword {
+    my ( $self, $keywordId, $bidAmt, $useGroupBidAmt, $adgroupId ) = @_;
+
+    return unless $keywordId;
+    return unless $bidAmt;
+    return unless $useGroupBidAmt;
+
+    my $params = $self->http->www_form_urlencode( { fields => 'bidAmt' } );
+    my $body = { bidAmt => $bidAmt, useGroupBidAmt => $useGroupBidAmt, nccAdgroupId => $adgroupId };
+    my $opts = { headers => { 'content-type' => 'application/json' }, content => encode_json($body) };
+    my $res = $self->request( 'PUT', "/ncc/keywords/$keywordId", $params, $opts );
+    return unless $res;
+    return $res->{content};
+}
+
+=head2 managedKeyword
+
+    GET /ncc/managedKeyword{?keywords}
+    my $json = $api->managedKeyword($keyword);
+
+=cut
+
+sub managedKeyword {
+    my ( $self, $keyword ) = @_;
+
+    return unless $keyword;
+
+    my $params = $self->http->www_form_urlencode( { keywords => $keyword } );
+    my $res = $self->request( 'GET', '/ncc/managedKeyword', $params );
+    return unless $res;
+    return $res->{content};
+}
+
+=head2 custom_headers
+
+=cut
 
 sub custom_headers {
     my ( $self, $method, $path ) = @_;
@@ -69,6 +178,10 @@ sub custom_headers {
 
     return { 'X-Timestamp' => $epoch, 'X-Signature' => $signature };
 }
+
+=head2 signature
+
+=cut
 
 sub signature {
     my ( $self, $epoch, $method, $path ) = @_;
