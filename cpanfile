30ccf494ff7ff6b4ffff6ea8b0335afdefff854d
Hyungsuk Hong <aanoaa@gmail.com>
2016-07-17 19:18:47 +0900

    Brand new module

---
+++ cpanfile
@@ -0,0 +1,5 @@
+requires 'Digest::SHA';
+requires 'Getopt::Long';
+requires 'HTTP::Tiny';
+requires 'Moo';
+requires 'Pod::Usage';
--- lib/WWW/Naver/SearchAd.pm
+++ lib/WWW/Naver/SearchAd.pm
@@ -1,275 +1,98 @@
 package WWW::Naver::SearchAd;
+
 # ABSTRACT: bidding(?) for http://searchad.naver.com
 
-use LWP::UserAgent;
-use HTTP::Cookies;
-use HTTP::Request;
-use HTTP::Request::Common;
+use Moo;
 
-use JSON::XS;
-use Try::Tiny;
+use Digest::SHA qw(hmac_sha256_base64);
+use HTTP::Tiny;
+use Time::HiRes qw(gettimeofday);
 
-my $HOST    = 'searchad.naver.com';
-my $REFERER = "http://$HOST";
+our $BASE_URL = 'https://api.naver.com';
 
-sub new {
-    my ($class, $args) = @_;
+has key         => ( is => 'ro', required => 1 );
+has secret      => ( is => 'ro', required => 1 );
+has customer_id => ( is => 'ro', required => 1 );
+has http        => (
+    is      => 'lazy',
+    default => sub {
+        my $self = shift;
+        return HTTP::Tiny->new( default_headers => { 'X-API-KEY' => $self->key, 'X-Customer' => $self->customer_id } );
+    }
+);
 
-    my $ua = LWP::UserAgent->new(
-        agent => $args->{agent} || 'Mozilla/5.0 (X11; Linux x86_64; rv:10.0.11) Gecko/20100101 Firefox/10.0.11 Iceweasel/10.0.11'
-    );
+=head1 METHODS
 
-    push @{ $ua->requests_redirectable }, 'POST';
-    $ua->cookie_jar(HTTP::Cookies->new);
-    $ua->add_handler(
-        'request_prepare' => sub {
-            my ($req, $ua, $h) = @_;
-            print $req->as_string, "\n" if $ENV{DEBUG};
-        }
-    );
+=head2 request
 
-    $ua->add_handler(
-        'response_done' => sub {
-            my ($res, $ua, $h) = @_;
-            print $res->as_string, "\n" if $ENV{DEBUG};
-        }
-    );
+    my $data = $api->request('GET', '/ncc/campaigns');
 
-    return bless {
-        agent => $ua
-    }, $class;
+=cut
+
+sub request {
+    my ( $self, $method, $path ) = @_;
+
+    print STDERR "--> Working on $method $path ... ";
+
+    my $res
+        = $self->http->request( $method, $BASE_URL . $path, { headers => $self->custom_headers( $method, $path ) } );
+
+    unless ( $res->{success} ) {
+        print STDERR "Failed\n";
+        print STDERR "! $res->{reason}\n";
+        print STDERR "! $res->{content}\n";
+        return;
+    }
+
+    print STDERR "OK\n";
+    return $res->{content};
 }
 
-sub signin {
-    my ($self, $username, $password) = @_;
+=head2 campagins
 
-    delete $self->{error};
-    if (!$username || !$password) {
-        $self->{error} = 'signin failed: no username or password';
-        return;
-    }
+    https://api.naver.com/ncc/campaigns
+    my $data = $api->campaigns(%attr);
 
-    my $ua  = $self->{agent};
-    my $res = $ua->request(
-        POST "https://$HOST/Login/login.json",
-        Referer => $REFERER,
-        Content => [
-            loginId  => $username,
-            loginPwd => $password
-        ]
-    );
+=cut
 
-    if (!$res->is_success) {
-        $self->{error} = $res->status_line;
-        return;
-    }
+sub campaigns {
+    my ( $self, $attr ) = @_;
 
-    my $data = try {
-        decode_json($res->content);
-    } catch {
-        $self->{error} = "failed to decode_json: $_";
-        return;
-    };
-
-    if ($data->{retCode} != 0) {
-        $self->{error} = $data->{retMsg};
-        return;
-    }
-
-    return $res->code;
+    ## TODO: $attr
+    return $self->request( 'GET', '/ncc/campaigns' );
 }
 
-sub get_bundles {
-    my $self = shift;
+sub custom_headers {
+    my ( $self, $method, $path ) = @_;
+    my $epoch = int( gettimeofday * 1000 );
+    my $signature = $self->signature( $epoch, $method, $path );
 
-    my $ua = $self->{agent};
-    my $res = $ua->request(GET "http://$HOST/AMCC30/AMCC3001_A01.nbp");
-
-    if (!$res->is_success) {
-        $self->{error} = $res->status_line;
-        return;
-    }
-
-    my $body = $res->content;
-    my ($json) = $body =~ m/bundleList: (.*?),?\r?\n/s;
-
-    unless ($json) {
-        $self->{error} = "Couldn't find bundleList";
-        return;
-    }
-
-    my $data = try {
-        decode_json($json);
-    } catch {
-        $self->{error} = "failed to decode_json: $_";
-        return;
-    };
-
-    return $data;
+    return { 'X-Timestamp' => $epoch, 'X-Signature' => $signature };
 }
 
-sub refresh {
-    my ($self, $bundle_id, $rank, $limit) = @_;
+sub signature {
+    my ( $self, $epoch, $method, $path ) = @_;
 
-    delete $self->{error};
-    if (!$bundle_id || !$rank) {
-        $self->{error} = "wrong arguments";
-        return;
+    my $secret = $self->secret;
+    my $data   = "$epoch.$method.$path";
+    my $digest = hmac_sha256_base64( $data, $secret );
+    while ( length($digest) % 4 ) {
+        $digest .= '=';
     }
 
-    my $data = $self->_get_bundle_data($bundle_id);
-    return if $self->{error};
-
-    my $params = $self->_set_prices_by_rank($data, $rank, $limit);
-    return if $self->{error};
-
-    $self->_apply_prices($params);
-    return if $self->{error};
-
-    return 1;
-}
-
-sub _get_bundle_data {
-    my ($self, $bundle_id) = @_;
-
-    my $ua = $self->{agent};
-    my $res = $ua->request(
-        POST "http://$HOST/AMCC30/AMCC3001_A02.json",
-        [
-            bundleId => $bundle_id
-        ]
-    );
-
-    if (!$res->is_success) {
-        $self->{error} = $res->status_line;
-        return;
-    }
-
-    my $data = try {
-        decode_json($res->content);
-    } catch {
-        $self->{error} = "failed to decode_json: $_";
-        return;
-    };
-
-    if ($data->{retCode} != 0) {
-        $self->{error} = $data->{retMsg};
-        return;
-    }
-
-    return $data;
-}
-
-sub _set_prices_by_rank {
-    my ($self, $decoded_data, $rank, $limit) = @_;
-
-    my %params;
-    my $i = 0;
-    for my $item (@{ $decoded_data->{cpcReqList} }) {
-        $params{"cpcReqList[$i][rowId]"} = 'r' . (0 + $i);
-        map { $params{"cpcReqList[$i][$_]"} = $item->{$_} } qw/urlId cpcGroupId cpcReqId keywordId keyword restrictType bidAmt isNxRestricted/;
-
-        $i++;
-    }
-
-    my $ua  = $self->{agent};
-    my $res = $ua->request(
-        POST "http://$HOST/AMCC23/AMCC2302_A04.json",
-        [
-            bidAmt => $limit || '100000',
-            rank   => $rank,
-            %params,
-        ]
-    );
-
-    if (!$res->is_success) {
-        $self->{error} = $res->status_line;
-        return;
-    }
-
-    my $data = try {
-        decode_json($res->content);
-    } catch {
-        $self->{error} = "failed to decode_json: $_";
-        return;
-    };
-
-    if ($data->{retCode} != 0) {
-        $self->{error} = $data->{retMsg};
-        return;
-    }
-
-    $i = 0;
-    for my $item (@{ $data->{rankCpcList} }) {
-        $params{"cpcReqList[$i][bidAmt]"} = $item->{bidAmt};
-        delete $params{"cpcReqList[$i][isNxRestricted]"};
-
-        $i++;
-    }
-
-    return \%params;
-}
-
-sub _apply_prices {
-    my ($self, $params) = @_;
-
-    my $ua  = $self->{agent};
-    my $res = $ua->request(
-        POST "http://$HOST/AMCC30/AMCC3001_A09.json",
-        Referer => $REFERER,
-        Content => [ %$params ]
-    );
-
-    if (!$res->is_success) {
-        $self->{error} = $res->status_line;
-        return;
-    }
-
-    my $data = try {
-        decode_json($res->content);
-    } catch {
-        $self->{error} = "failed to decode_json: $_";
-        return;
-    };
-
-    if ($data->{retCode} != 0) {
-        $self->{error} = $data->{retMsg};
-        return;
-    }
-
-    return $res->code;
+    return $digest;
 }
 
 1;
 
 =pod
 
+=encoding utf-8
+
 =head1 NAME
 
 WWW::Naver::SearchAd - bidding(?) for http://searchad.naver.com
 
 =head1 SYNOPSIS
-
-    $ node salogin.js `password`    # requires node
-    # 99fc288bed... is used as a password for signin
-
-    use WWW::Naver::SearchAd;
-    my $ad = WWW:Naver::SearchAd->new;
-    die $ad->{error} unless $ad->signin('username', '99fc288bed...');
-
-    my $bundle_id = '11111';
-
-    my $rank      = 11
-    # 1  .. 10 : Power1 .. Power10
-    # 11 .. 15 : Biz1 .. Biz5
-
-    die $ad->{error} unless $ad->refresh($bundle_id, $rank);
-
-
-    $ export DEBUG=1    # for debugging HTTP Messages
-
-=head1 DESCRIPTION
-
-no description yet.
 
 =cut
